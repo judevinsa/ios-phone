5faf6595b932428d189f4ef5ff56ba958a69b1c4
Julien Rollet <julien.rollet@applidium.com>
2015-04-30 12:23:26 +0200

    Initializes repository

---
+++ ios-phone/ios-phone/IPAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-phone
+//
+//  Created by Julien Rollet on 30/04/15.
+//  Copyright (c) 2015 Julien Rollet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-phone/ios-phone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios-phone
+//
+//  Created by Julien Rollet on 30/04/15.
+//  Copyright (c) 2015 Julien Rollet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "IPAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IPAppDelegate class]));
+    }
+}
